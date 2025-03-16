deee487323f5b729538f82a526e7b4a9eaec9d59
Emmanuel Paatz <emmanuel.paatz@gmail.com>
2025-03-16 16:03:14 +0100

    refetch reviewItems when list is almost empty

--- fluent-frontend/src/flashcards/components/Review.tsx
+++ fluent-frontend/src/flashcards/components/Review.tsx
@@ -9,7 +9,21 @@ function Review() {
     ConfigContext
   ) as Context;
 
-  useEffect(() => {
+  useEffect(() => fetchNewReviewItems(), []);
+
+  const nextConversation = (success: boolean): void => {
+    const currenConversation = reviewList[0];
+    if (success) {
+      setReviewList((reviewList) => reviewList.slice(1));
+      updateConversationReviewStatus(currenConversation);
+      if (reviewList.length <= 1) fetchNewReviewItems();
+    } else {
+      currenConversation.alreadyFailed = true;
+      setReviewList((reviewList) => [...reviewList.slice(1), currenConversation]);
+    }
+  };
+
+  const fetchNewReviewItems = () => {
     getReviewList().then((newReviewList: Conversation[]) =>
       setReviewList((reviewList) =>
         updateCacheWithNewConversations(reviewList, newReviewList, targetLanguage).map((reviewItem) => ({
@@ -18,18 +32,7 @@ function Review() {
         }))
       )
     );
-  }, []);
-
-  const nextConversation = (success: boolean): void => {
-    const currenConversation = reviewList[0];
-    if (success) {
-      setReviewList((reviewList) => reviewList.slice(1));
-      updateConversationReviewStatus(currenConversation);
-    } else {
-      currenConversation.alreadyFailed = true;
-      setReviewList((reviewList) => [...reviewList.slice(1), currenConversation]);
-    }
-  };
+  }
 
   return reviewList.length === 0 ? (
     <div>Nothing to review</div>
