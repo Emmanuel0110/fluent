24a60257a4ab5b26bc58f8588b5daf27cfd20a8c
Emmanuel Paatz <emmanuel.paatz@gmail.com>
2025-10-12 23:21:25 +0200

    fix bug unsubscribed conversation still shows up

--- fluent-backend/src/controlers/reviewControlers.js
+++ fluent-backend/src/controlers/reviewControlers.js
@@ -91,7 +91,7 @@ async function getKnownConversationsForWords(wordIds, userLearningData) {
   console.log(`🔍 Debug - conversations: ${conversations}`);
   const userConversations = userLearningData.conversations;
   const knownConversations = conversations.reduce((acc, conversation) => {
-    const lastReviewDate = userConversations.find(({ _id }) => (_id = conversation._id))?.lastReviewDate;
+    const lastReviewDate = userConversations.find(({ _id }) => _id === conversation._id)?.lastReviewDate;
     if (lastReviewDate) {
       acc.push({ ...conversation, lastReviewDate });
     }
