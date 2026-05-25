e7fc7776cb19143a8258f109b22f5a917f52b029
Emmanuel Paatz <emmanuel.paatz@gmail.com>
2026-05-25 18:38:33 +0200

    improve css review

--- fluent-frontend/src/components/ReviewSentence.tsx
+++ fluent-frontend/src/components/ReviewSentence.tsx
@@ -26,15 +26,12 @@ function ReviewSentence({ index, currentSentenceNumber, answerRevealed, conversa
       <div className="clickable-sentence" onClick={() => handleSentenceClick(index, "source")}>
         {conversation.multiLingualSentences[index].sourceLanguage.text}
       </div>
-      <div className="translationSpaceholder">
-        {answerRevealed && (
-          <div className="clickable-sentence bold" onClick={() => handleSentenceClick(index, "target")}>
-            {targetText}
-            <TextToSpeech text={targetText} language={getLanguageLabel(targetLanguage)} />
-          </div>
-        )}
+      <div className={`translationSpaceholder${answerRevealed ? "" : " hidden"}`}>
+        <div className="clickable-sentence bold" onClick={() => handleSentenceClick(index, "target")}>
+          {targetText}
+          {answerRevealed && <TextToSpeech text={targetText} language={getLanguageLabel(targetLanguage)} />}
+        </div>
       </div>
-      <br />
     </div>
   );
 }
